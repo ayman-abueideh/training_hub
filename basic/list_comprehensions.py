3bda3eca6758cbc4ae7c603c3d0f1730cdf42d7f
ayman-abueideh <ayman-abu-eideh@hotmail.com>
2020-03-12 15:38:51 +0200

    ad1

--- basic/list_comprehensions.py
+++ basic/list_comprehensions.py
@@ -12,8 +12,8 @@ print(word_lengths)
 
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
-word_lengths = [len(word) for word in words if word != "the"]
-print(words)
+# word_lengths = [len(word) for word in words if word != "the"]
+# print(words)
 print(word_lengths)
 
 sentence = "the quick brown fox jumps over the lazy dog"
