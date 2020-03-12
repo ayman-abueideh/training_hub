ce64625f8be0ad9bfb9dbaa561c68ef3651bdbae
ayman-abueideh <ayman-abu-eideh@hotmail.com>
2020-03-12 15:44:43 +0200

    ad1

--- basic/list_comprehensions.py
+++ basic/list_comprehensions.py
@@ -14,6 +14,7 @@ sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 # word_lengths = [len(word) for word in words if word != "the"]
 # print(words)
+#asdfas
 print(word_lengths)
 
 sentence = "the quick brown fox jumps over the lazy dog"
